77235e9fc105c0d3223943c87c376ca6a87ad1de
Akijudo <akijudo@proton.me>
2022-10-14 03:30:13 +0000

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221014.030344';
+const cacheName = 'chirpy-20221014.032959';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
